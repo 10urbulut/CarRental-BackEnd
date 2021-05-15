df0d9e4fbbca33c0577464a3ce151e46d2e07d45
10urb <10urbulut@gmail.com>
2021-05-15 19:21:00 +0300

    fluent validation support

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,65 +24,58 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            if (car.Description.Length > 2 && car.DailyPrice > 0)
-            {
-                _carDal.Add(car);
-                return new SuccessResult(Messages.ContentAdded);
-
-            }
-            
-            return new ErrorResult(Messages.ContentNameInvalid);
-
-
+            ValidationTool.Validate(new CarValidator(), car);
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarReturnError);
         }
 
         public IResult Delete(Car car)
         {
-            
+
             _carDal.Delete(car);
             return new SuccessResult(Messages.ContentDeleted);
         }
 
         public IDataResult<List<Car>> GetAll()
         {
-            
+
             return new SuccessDataResult<List<Car>>
-                (_carDal.GetAll(),Messages.ContentsListed);
+                (_carDal.GetAll(), Messages.ContentsListed);
         }
 
         public IDataResult<List<Car>> GetCarsByBrandId(int id)
         {
-            return new SuccessDataResult<List<Car>> (_carDal.GetAll(c=>c.BrandId==id),Messages.ContentsListed);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.BrandId == id), Messages.ContentsListed);
         }
 
         public IDataResult<List<Car>> GetCarsByColorId(int id)
         {
-            return new SuccessDataResult<List<Car>>( _carDal.GetAll(c=> c.ColorId==id),Messages.ContentsListed);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == id), Messages.ContentsListed);
         }
 
         public IDataResult<List<CarDetailDto>> GetCarDetails()
         {
-            return new SuccessDataResult<List<CarDetailDto>>( _carDal.GetCarDetails(),Messages.ContentsListed);
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails(), Messages.ContentsListed);
         }
 
         public IResult Update(Car car)
         {
             _carDal.Update(car);
 
-            return new SuccessResult( Messages.ContentUpdated);
-            
+            return new SuccessResult(Messages.ContentUpdated);
+
         }
 
-       
+
 
         public IDataResult<List<Car>> GetCarByDailyPrice(decimal min, decimal max)
         {
-            return new SuccessDataResult<List<Car>>( _carDal.GetAll(c=>c.DailyPrice>=min && c.DailyPrice<=max),Messages.ContentsListed);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.DailyPrice >= min && c.DailyPrice <= max), Messages.ContentsListed);
         }
 
         public IDataResult<Car> GetCarByCarId(int id)
         {
-            return new SuccessDataResult<Car>( _carDal.Get(c=>c.CarId==id),Messages.ContentsListed);
+            return new SuccessDataResult<Car>(_carDal.Get(c => c.CarId == id), Messages.ContentsListed);
         }
     }
 }
---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.Description).MinimumLength(2);
+            RuleFor(c => c.Description).NotEmpty();
+            RuleFor(c => c.ModelYear).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+
+            //günlük fiyat model 2020 olduğunda 200 veya daha büyük olmalı
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(200).When(c => c.ModelYear == 2020);
+           //RuleFor(c => c.Description).Must(StartWithA).WithMessage("Ürünler A ile başlamalı");
+        }
+
+       /* private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }*/
+    }
+}
